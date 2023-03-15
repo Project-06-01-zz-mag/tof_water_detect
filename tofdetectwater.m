6c689b8debfce492568c635295d84592da23f6de
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-03-15 17:07:02 +0800

    简化code和变量处理

--- tofdetectwater.m
+++ tofdetectwater.m
@@ -1,39 +1,18 @@
-% clc
-% clear
-% close all
+clc
+clear
+close all
 
 % tofampdpsfload = load('tofdpsf1.mat');
-tofampdpsfload = load('huioutrise.mat');
-tofampdpsf=tofampdpsfload.huioutorise;
-tof_data = tofampdpsf.VarName1;
-length = size(tof_data);
+dpfs_mat_load = load('huioutrise.mat');     %载入mat数据
+dpfs_mat_select=dpfs_mat_load.huioutorise;  %选择mat
+dpfs_data = dpfs_mat_select.VarName1;       %选择列
+
+length = size(dpfs_data);
 win_size = 500;
 
-tof_var=zeros(501,5000)
-result=zeros(45505,1)
-
-n = 0:win_size; % 采样序列
-
-figure
-% subplot(2,1,1)
-hold on
-plot(tof_data)
-hold off
-legend("tofampdpsf")
-
-% for i = 1:1000:length-win_size
-%     tof_var(win_size,i) = fft(tof_data(i:i+win_size));
-% end
-
 for i = 1:win_size:length-win_size
-    tof_var(:,i) = fft(tof_data(i:i+win_size));
-    M = abs(tof_var(:,i));
-    
-%     subplot(2,1,2)
-%     hold on
-%     plot(n,M)
-%     hold off
-%     legend("tof var")
+    y= fft(dpfs_data(i:i+win_size)); %fft计算
+    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增
 
     if M(499) > 500
        for a = i:i+win_size
@@ -44,16 +23,12 @@ for i = 1:win_size:length-win_size
         result(a)=0;         
        end
     end
-    
-%     subplot(2,1,2)
-
-    
 end
 
+figure
+plot(dpfs_data)
 hold on
 plot(result)
-hold off
-legend("tof var")
 
 % figure
 
