11a471697c7a75f58270997db45eabc42a5db31a
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-03-16 11:19:59 +0800

    同时显示两张图

--- tofdetectwater.m
+++ tofdetectwater.m
@@ -33,23 +33,46 @@ for i = 1:win_size:length-win_size
 end
 
 figure
+subplot(2,1,1)
 plot(dpfs_data)
 hold on
 plot(result)
 hold on
 plot(value)
 
-% figure
+% tofampdpsfload = load('tofdpsf1.mat');
+dpfs_mat_load = load('rawdpfs_ground1.mat');     %载入mat数据
+dpfs_mat_select=dpfs_mat_load.rawdpfsground1;  %选择mat
+dpfs_data = dpfs_mat_select.VarName1;       %选择列
 
-% subplot(2,1,1)
-% hold on
-% plot(tof_data)
-% hold off
-% legend("tofampdpsf")
+length = size(dpfs_data);
 
-% subplot(2,1,2)
-% hold on
-% plot(tof_var)
-% hold off
-% legend("tof var")
+n = 0:win_size; % 采样序列
+
+for i = 1:win_size:length-win_size
+    y= fft(dpfs_data(i:i+win_size)); %fft计算
+    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增
+
+    sum_result= sum(M(400:500))/10000;
+    for a = i:i+win_size
+        value(a)=sum_result;       
+    end
+
+    if M(499) > 500
+       for a = i:i+win_size
+        result(a)=1*(-80);         
+       end
+    else
+       for a = i:i+win_size
+        result(a)=0;         
+       end
+    end
+end
+
+subplot(2,1,2)
+plot(dpfs_data)
+hold on
+plot(result)
+hold on
+plot(value)
 
