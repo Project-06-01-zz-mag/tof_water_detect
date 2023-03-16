0d17b212ceb6eaafc5fa15f3554a95c9e55152b2
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-03-16 11:23:13 +0800

    同时显示两张图，一个是水面上，一个在地面上

--- tofdetectwater.m
+++ tofdetectwater.m
@@ -41,8 +41,8 @@ hold on
 plot(value)
 
 % tofampdpsfload = load('tofdpsf1.mat');
-dpfs_mat_load = load('rawdpfs_ground1.mat');     %载入mat数据
-dpfs_mat_select=dpfs_mat_load.rawdpfsground1;  %选择mat
+dpfs_mat_load = load('rawdpfs_water1.mat');     %载入mat数据
+dpfs_mat_select=dpfs_mat_load.rawdpfswater1;  %选择mat
 dpfs_data = dpfs_mat_select.VarName1;       %选择列
 
 length = size(dpfs_data);
