6da7e744cfc34c5571b9d7119a4c211635eb65c3
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-03-16 15:19:19 +0800

    通过一定时间的信号强度持续超过limit判断是否在水面上

--- tofdetectwater.m
+++ tofdetectwater.m
@@ -2,65 +2,48 @@ clc
 clear
 close all
 
-
-frequency_spectrum_x1 = 400
-frequency_spectrum_x2 = 500
-
-scale = 1000
-
-figure_row = 3
+frequency_spectrum_x1 = 400 %信号频率窗口下边界
+frequency_spectrum_x2 = 500 %信号频率窗口上边界
+scale = 1000 %fft信号幅值求和后的缩放
+sum_value_limit = 6  %信号求和后认为是水面的信号和限值
+water_cnt = 0 ; %判断可能出现在水面上的次数
+water_cnt_limit = 3
+figure_row = 2
 figure_column = 1
+win_size = 500; % 原始信号进行fft的窗口大小
 
-dpfs_mat_load = load('rawdpfs_ground1.mat');     %载入mat数据
+dpfs_mat_load = load('rawdpfs_ground1.mat');   %载入mat数据
 dpfs_mat_select=dpfs_mat_load.rawdpfsground1;  %选择mat
-dpfs_data = dpfs_mat_select.VarName1;       %选择列
+dpfs_data = dpfs_mat_select.VarName1;          %选择列
+length = size(dpfs_data);                      %求s原始信号的长度
 
-length = size(dpfs_data);
-
-win_size = 500;
-n = 0:win_size; % 采样序列
-
-figure
-subplot(2,1,1)
-plot(dpfs_data)
-hold on
-
-Fs=1000;
-Fs2=Fs/2;
-[b,a]=butter(4,50/Fs2,'high');
-y=filter(b,a,dpfs_data);%经过filter滤波之后得到的数据y则是经过带通滤波后的信号数据
-subplot(2,1,2)
-plot(y)
-hold on
+%对原始信号进行脉冲噪声去噪
+for i = 2:length
+    if (dpfs_data(i) < (-80)||dpfs_data(i) ==0)
+        dpfs_data(i) = dpfs_data(i-1);
+    end
+end 
+%对原始信号进行中值滤波
+%dpfs_data_filter = medfilt1(dpfs_data,100);
 
 for i = 1:win_size:length-win_size
     y= fft(dpfs_data(i:i+win_size)); %fft计算
-    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增
-    
-    for a = 1:i
-        select(a) = 0;
-    end
-    for a = i:i+win_size
-        select(a) = -80;
-    end
-    select(a) = 0
-    
-    subplot(2,1,1)
-    plot(dpfs_data)
-    hold on
-    plot(select)
-    hold off
-        
-    subplot(2,1,2)
-    plot(n,M)
-    hold off
+    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增       
 
+    %对选定的频率范围进行求和
     sum_result= sum(M(frequency_spectrum_x1:frequency_spectrum_x2))/scale;
     for a = i:i+win_size
         value(a)=sum_result;       
     end
 
-    if M(499) > 500
+    if(sum_result > sum_value_limit)
+        water_cnt = water_cnt +1;
+    else
+        water_cnt = 0;
+    end
+
+
+    if water_cnt>water_cnt_limit
        for a = i:i+win_size
         result(a)=1*(-80);         
        end
@@ -80,24 +63,39 @@ hold on
 plot(value)
 hold on
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+dpfs_mat_load = load('rawdpfs_water1.mat');     %载入mat数据
+dpfs_mat_select=dpfs_mat_load.rawdpfswater1;  %选择mat
+dpfs_data = dpfs_mat_select.VarName1;       %选择列
+length = size(dpfs_data);
+
 for i = 2:length
     if (dpfs_data(i) < (-80)||dpfs_data(i) ==0)
         dpfs_data(i) = dpfs_data(i-1);
     end
 end 
-
-dpfs_data_filter = medfilt1(dpfs_data,100);
+%对原始信号进行中值滤波
+%dpfs_data_filter = medfilt1(dpfs_data,100);
 
 for i = 1:win_size:length-win_size
     y= fft(dpfs_data(i:i+win_size)); %fft计算
-    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增
+    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增       
 
+    %对选定的频率范围进行求和
     sum_result= sum(M(frequency_spectrum_x1:frequency_spectrum_x2))/scale;
     for a = i:i+win_size
         value(a)=sum_result;       
     end
 
-    if M(499) > 500
+    if(sum_result > sum_value_limit)
+        water_cnt = water_cnt +1;
+    else
+        water_cnt = 0;
+    end
+
+
+    if water_cnt > water_cnt_limit
        for a = i:i+win_size
         result(a)=1*(-80);         
        end
@@ -109,42 +107,6 @@ for i = 1:win_size:length-win_size
 end
 
 subplot(figure_row,figure_column,2)
-plot(dpfs_data_filter)
-hold on
-plot(result)
-hold on
-plot(value)
-hold on
-
-dpfs_mat_load = load('rawdpfs_water1.mat');     %载入mat数据
-dpfs_mat_select=dpfs_mat_load.rawdpfswater1;  %选择mat
-dpfs_data = dpfs_mat_select.VarName1;       %选择列
-
-length = size(dpfs_data);
-
-n = 0:win_size; % 采样序列
-
-for i = 1:win_size:length-win_size
-    y= fft(dpfs_data(i:i+win_size)); %fft计算
-    M = abs(y);         %求信号幅度 列向量 ，频率从小到大递增
-
-    sum_result= sum(M(frequency_spectrum_x1:frequency_spectrum_x2))/scale;
-    for a = i:i+win_size
-        value(a)=sum_result;       
-    end
-
-    if M(499) > 500
-       for a = i:i+win_size
-        result(a)=1*(-80);         
-       end
-    else
-       for a = i:i+win_size
-        result(a)=0;         
-       end
-    end
-end
-
-subplot(figure_row,figure_column,3)
 plot(dpfs_data)
 hold on
 plot(result)
