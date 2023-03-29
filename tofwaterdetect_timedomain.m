e6da0ccf4503ea6a35d4eddb8ec4656f38ceb8ba
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-03-29 16:41:47 +0800

    调节阈值

--- tofwaterdetect_timedomain.m
+++ tofwaterdetect_timedomain.m
@@ -11,8 +11,8 @@ win_size = 30;        % 原始信号进行fft的窗口大小
 step_size = 1         % 步进长度
 figure_row = 1        % 绘图的row numble
 figure_column = 1     % 绘图的column numble
-std_limit_value = 0.7 % 判断是否为水面上的标准差阈值
-water_cnt_limit = 3   % 判断是否为水面上的连续次数阈值
+std_limit_value = 0.3 % 判断是否为水面上的标准差阈值
+water_cnt_limit = 2   % 判断是否为水面上的连续次数阈值
 water_cnt = 0 ;       % 判断可能出现在水面上的次数
 
 %滤波器参数
