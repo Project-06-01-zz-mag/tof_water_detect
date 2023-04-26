5c061b77db31dc058459eccc3603a2caebbbbd43
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-04-26 15:20:07 +0800

    [scripe]add water vs asphaltroad

--- 3_verify_script/water_and_grand.m
+++ 3_verify_script/water_and_grand.m
@@ -21,57 +21,58 @@ scale_value = 10
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_grass.mat');  
-dpfs_mat_select_grass = dpfs_mat_struct_load.truedata;
+% dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_grass.mat');  
+% dpfs_mat_select_grass = dpfs_mat_struct_load.truedata;
 
-% length = size(dpfs_mat_select_grass,1)
+% % length = size(dpfs_mat_select_grass,1)
         
 
-for i = win_size+time_begin:step_size:time_end-win_size
-    %下面实际上获得的时间窗口内滤波的值，比如长度31，那么滤波后也是31
-    inputdata_filter_ = filter(b2,a2,dpfs_mat_select_grass(i-win_size:i));%inputdata_filter_输出的大小是win_size+1个数据
+% for i = win_size+time_begin:step_size:time_end-win_size
+%     %下面实际上获得的时间窗口内滤波的值，比如长度31，那么滤波后也是31
+%     inputdata_filter_ = filter(b2,a2,dpfs_mat_select_grass(i-win_size:i));%inputdata_filter_输出的大小是win_size+1个数据
     
-    %获取时间窗口内的信号峰峰值
-    bsort = sort(dpfs_mat_select_grass(i-win_size:i),"ascend");
-    a =abs(1/ (bsort(1) -(bsort(win_size+1) )))
+%     %获取时间窗口内的信号峰峰值
+%     bsort = sort(dpfs_mat_select_grass(i-win_size:i),"ascend");
+%     a =abs(1/ (bsort(1) -(bsort(win_size+1) )))
 
-    %下面将滤波后的最后一个值变为滤波处理过的值？，同时乘上缩放
-    if (a<0.1)
-        after_filter_data(i) = inputdata_filter_(win_size+1) * a
-        deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
-    else
-        after_filter_data(i) = inputdata_filter_(win_size+1)
-        deviation = std(after_filter_data(i-win_size:i),'omitnan')
-    end 
+%     %下面将滤波后的最后一个值变为滤波处理过的值？，同时乘上缩放
+%     if (a<0.1)
+%         after_filter_data(i) = inputdata_filter_(win_size+1) * a
+%         deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
+%     else
+%         after_filter_data(i) = inputdata_filter_(win_size+1)
+%         deviation = std(after_filter_data(i-win_size:i),'omitnan')
+%     end 
 
-    result(i) = deviation * scale_value
-end
+%     result(i) = deviation * scale_value
+% end
 
-for i = win_size+time_begin:step_size:time_end-win_size
-    deviation = std(result(i-win_size:i),'omitnan')
-    result2(i) = deviation
-end
+% for i = win_size+time_begin:step_size:time_end-win_size
+%     deviation = std(result(i-win_size:i),'omitnan')
+%     result2(i) = deviation
+% end
 
-subplot(figure_row,figure_column,1)
+% subplot(figure_row,figure_column,1)
 
 
-plot(dpfs_mat_select_grass)  
-hold on
-plot(result)    
-hold on
-plot(result2,'g')
-hold on
+% plot(dpfs_mat_select_grass)  
+% hold on
+% plot(result)    
+% hold on
+% plot(result2,'g')
+% hold on
 
-title('Asphalt pavement')
-xlabel('time(1s/100point)')
-ylabel('lg(amp)')
+% title('Asphalt pavement')
+% xlabel('time(1s/100point)')
+% ylabel('lg(amp)')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dpfs_mat_struct_load = load('origindata/rawdpfs_water1.mat');   
-dpfs_mat_select_water = dpfs_mat_struct_load.rawdpfswater1.VarName1;
+dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_water1_origin.mat');   
+dpfs_mat_select_water = dpfs_mat_struct_load.origindata;
 
-time_begin_hebian = 8909
-time_end_hebian = time_begin_hebian + time_end - time_begin 
+time_begin_hebian = 6647
+%time_end_hebian = time_begin_hebian + time_end - time_begin 
+time_end_hebian = time_begin_hebian + 2000
 
 % for i = win_size+time_begin:step_size:time_end-win_size
 %     deviation = std(dpfs_mat_select_water(i-win_size:i),'omitnan')
@@ -92,7 +93,9 @@ time_end_hebian = time_begin_hebian + time_end - time_begin
 % plot(result2,'g')
 % hold on
 
-
+b = 0
+count = 0
+count1 = 100
 for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
     %下面实际上获得的时间窗口内滤波的值，比如长度是31，那么滤波后也是31
     inputdata_filter_ = filter(b2,a2,dpfs_mat_select_water(i-win_size:i));%inputdata_filter_输出的大小是win_size+1个数据
@@ -100,6 +103,26 @@ for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
     %获取时间窗口内的信号峰峰值
     bsort = sort(dpfs_mat_select_water(i-win_size:i),"ascend");
     a =abs(1/ (bsort(1) -(bsort(win_size+1) )))
+
+    if (a<0.1)
+        count = count + 1
+    else
+        count = 0
+    end
+
+    if(count > 100)
+        b = 1;
+    end
+
+    if(b == 1)
+        a = 1
+        count1 = count1 - 1 
+    end
+
+    if (count1 == 0)
+        b = 0;
+        count1 = 100;
+    end
 
     %下面将滤波后的最后一个值变为滤波处理过的值？，同时乘上缩放值
     if (a<0.1)
@@ -120,7 +143,8 @@ end
 
 subplot(figure_row,figure_column,2)
 
-
+plot(after_filter_data)  
+hold on
 plot(dpfs_mat_select_water)  
 hold on
 plot(result)    
@@ -133,15 +157,40 @@ xlabel('time(1s/100point)')
 ylabel('lg(amp)')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dpfs_mat_struct_load = load('origindata/rawdpfs_hebian.mat');   
-dpfs_mat_select_water = dpfs_mat_struct_load.hebian.VarName1;
+% dpfs_mat_struct_load = load('origindata/rawdpfs_hebian.mat');   
+% dpfs_mat_select_water = dpfs_mat_struct_load.hebian.VarName1;
 
-time_begin_hebian = 7241
-time_end_hebian = time_begin_hebian + time_end - time_begin 
+% time_begin_hebian = 7241
+% time_end_hebian = time_begin_hebian + time_end - time_begin 
+
+% % for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
+% %     deviation = std(dpfs_mat_select_water(i-win_size:i),'omitnan')
+% %     result(i) = deviation
+% % end
+
+% % for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
+% %     deviation = std(result(i-win_size:i),'omitnan')
+% %     result2(i) = deviation
+% % end
 
 % for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
-%     deviation = std(dpfs_mat_select_water(i-win_size:i),'omitnan')
-%     result(i) = deviation
+%     %下面实际上获得的时间窗口内滤波的值，比如长度31，那么滤波后也是31
+%     inputdata_filter_ = filter(b2,a2,dpfs_mat_select_water(i-win_size:i));%inputdata_filter_输出的大小是win_size+1个数据
+    
+%     %获取时间窗口内的信号峰峰
+%     bsort = sort(dpfs_mat_select_water(i-win_size:i),"ascend");
+%     a =abs(1/ (bsort(1) -(bsort(win_size+1) )))
+
+%     %下面将滤波后的最后一个值变为滤波处理过的值？，同时乘上缩放
+%     if (a<0.1)
+%         after_filter_data(i) = inputdata_filter_(win_size+1) * a
+%         deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
+%     else
+%         after_filter_data(i) = inputdata_filter_(win_size+1)
+%         deviation = std(after_filter_data(i-win_size:i),'omitnan')
+%     end 
+
+%     result(i) = deviation * scale_value
 % end
 
 % for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
@@ -149,41 +198,85 @@ time_end_hebian = time_begin_hebian + time_end - time_begin
 %     result2(i) = deviation
 % end
 
-for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
-    %下面实际上获得的时间窗口内滤波的值，比如长度31，那么滤波后也是31
-    inputdata_filter_ = filter(b2,a2,dpfs_mat_select_water(i-win_size:i));%inputdata_filter_输出的大小是win_size+1个数据
-    
-    %获取时间窗口内的信号峰峰
-    bsort = sort(dpfs_mat_select_water(i-win_size:i),"ascend");
-    a =abs(1/ (bsort(1) -(bsort(win_size+1) )))
+% subplot(figure_row,figure_column,3)
 
-    %下面将滤波后的最后一个值变为滤波处理过的值？，同时乘上缩放
-    if (a<0.1)
-        after_filter_data(i) = inputdata_filter_(win_size+1) * a
-        deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
-    else
-        after_filter_data(i) = inputdata_filter_(win_size+1)
-        deviation = std(after_filter_data(i-win_size:i),'omitnan')
-    end 
+% plot(dpfs_mat_select_water)  
+% hold on
+% plot(result)    
+% hold on
+% plot(result2,'g')
+% hold on
 
-    result(i) = deviation * scale_value
-end
-
-for i = win_size+time_begin_hebian:step_size:time_end_hebian-win_size
-    deviation = std(result(i-win_size:i),'omitnan')
-    result2(i) = deviation
-end
-
-subplot(figure_row,figure_column,3)
-
-plot(dpfs_mat_select_water)  
-hold on
-plot(result)    
-hold on
-plot(result2,'g')
-hold on
-
-title('water2')
-xlabel('time(1s/100point)')
-ylabel('lg(amp)')
+% title('water2')
+% xlabel('time(1s/100point)')
+% ylabel('lg(amp)')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %数据处理函数
+% function myFun(inputdata,figure_num)
+    
+%     global win_size step_size figure_row figure_column 
+%     global std_limit_value water_cnt_limit water_cnt 
+%     global b2 a2 %滤波器传递函数的系数
+
+%     length = size(inputdata,1);
+% %     j = 1
+% %     for i=1:3:length
+% %         origindata(j) = inputdata(i)
+% %         j= j+1
+% %     end
+
+%     %限幅滤波
+%     for i = 2:length
+%         if (inputdata(i) < (-70)||inputdata(i)>-13)
+%             inputdata(i) = inputdata(i-1);
+%         end
+%     end 
+
+%     % for i = 2:length
+%     %     if (abs(inputdata(i-1) - inputdata(i))> 20)
+%     %         inputdata(i) = inputdata(i-1);
+%     %     end
+%     % end 
+%     subplot(figure_row,figure_column,figure_num)
+%     for i = win_size+1:step_size:length-win_size
+%         inputdata_filter_ = filter(b2,a2,inputdata(i-win_size:i));%经过filter滤波之后得到的数据y则是经过带通滤波后的信号数据
+        
+%         bsort = sort(inputdata(i-win_size:i),"ascend");
+%         a =abs(1/ (bsort(1) -(bsort(31) )))
+
+%         if (a<0.1)
+%             after_filter_data(i) = inputdata_filter_(31) * a
+%             deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
+%         else
+%             after_filter_data(i) = inputdata_filter_(31)
+%             deviation = std(after_filter_data(i-win_size:i),'omitnan')
+%         end 
+    
+%         result(i) = deviation
+%         if(deviation > std_limit_value)
+%             water_cnt = water_cnt +1;
+%         else
+%             water_cnt = 0
+%         end
+
+%         if water_cnt>water_cnt_limit            
+%             water_flag(i)=1*(-80);                     
+%          else
+%             water_flag(i)=0;         
+%          end        
+%     end
+
+%     plot(inputdata)  %滤波前的时域图
+%     hold on
+%     plot(result)     %对应点的标准差图
+%     hold on
+%     plot(water_flag) %对应点是否在水面的标志位输出
+%     hold on
+%     plot(after_filter_data) %滤波后的数据
+%     hold on
+% end
---
+++ 3_verify_script/water_vs_asphaltroad.m
@@ -0,0 +1,122 @@
+clc
+clear
+close all
+
+% dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_water1_origin.mat');   
+% dpfs_mat_select_water = dpfs_mat_struct_load.origindata;
+% time_begin_hebian = 6647
+% picture_location = 1
+% myFun(dpfs_mat_select_water,picture_location,time_begin_hebian,time_begin_hebian+1000)
+
+dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_grass.mat');   
+dpfs_mat_select_water = dpfs_mat_struct_load.truedata;
+time_begin_hebian = 1000
+picture_location = 2
+myFun(dpfs_mat_select_water,picture_location,time_begin_hebian,time_begin_hebian+5000)
+
+% dpfs_mat_struct_load = load('1_truesensordata/rawdpfs_water2_origin.mat');   
+% dpfs_mat_select_water = dpfs_mat_struct_load.origindata;
+% time_begin_hebian = 200
+% picture_location = 3
+% myFun(dpfs_mat_select_water,picture_location,time_begin_hebian,time_begin_hebian+1000)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%数据处理函数
+function myFun(inputdata,figure_num,time_begin,time_end)
+
+    win_size = 30;  
+    step_size = 2 
+    figure_row = 3  
+    figure_column =1
+    scale_value =10 %结果缩放
+
+    %滤波器参数
+    Fs = 33
+    high_pass = 10
+    Wc=2*high_pass/Fs;            % 截止频率 10Hz
+    [b2,a2]=butter(4,Wc,'high');  % 四阶的巴特沃斯高通滤波
+
+    b = 0           % 控制极值缩放的开关
+    count = 0       % 极值个数阈值
+    count1 = 100    % 反极值个数阈值
+
+    result2_value_threshold = 5      %二次标准差的阈值
+    result2_over_threshold_count_now = 0 %二次标准差的超阈值次数
+    result2_count_threshold = 33     %超过阈值的次数
+
+    length = size(inputdata,1); % 获取输入数据的长度
+    for i = 2:length            % 限幅滤波
+        if (inputdata(i) < (-70)||inputdata(i)>-13)
+            inputdata(i) = inputdata(i-1);
+        end
+    end 
+
+    for i = win_size+time_begin:step_size:time_end-win_size
+        inputdata_filter_ = filter(b2,a2,inputdata(i-win_size:i));%经过filter滤波之后得到的数据y则是经过带通滤波后的信号数据
+        
+        bsort = sort(inputdata(i-win_size:i),"ascend");
+        a =abs(1/ (bsort(1) -(bsort(win_size+1))))
+
+        % if (a<0.1)
+        %     count = count + 1
+        % else
+        %     count = 0
+        % end
+    
+        % if(count > 100)
+        %     b = 1;
+        % end
+    
+        % if(b == 1)
+        %     a = 1
+        %     count1 = count1 - 1 
+        % end
+    
+        % if (count1 == 0)
+        %     b = 0;
+        %     count1 = 100;
+        % end
+
+        % if (a<0.1)
+        %     after_filter_data(i) = inputdata_filter_(win_size+1)
+        %     deviation = std(after_filter_data(i-win_size:i),'omitnan') * a
+        % else
+        %     after_filter_data(i) = inputdata_filter_(win_size+1)
+        %     deviation = std(after_filter_data(i-win_size:i),'omitnan')
+        % end 
+    
+        after_filter_data(i) = inputdata_filter_(win_size+1)
+        deviation = std(after_filter_data(i-win_size:i),'omitnan')
+
+        result(i) = deviation * scale_value  
+    end
+
+    for i = win_size+time_begin:step_size:time_end-win_size
+        deviation = std(result(i-win_size:i),'omitnan')
+        result2(i) = deviation
+
+        if (result2(i) > result2_value_threshold)
+            result2_over_threshold_count_now = result2_over_threshold_count_now +1
+        else
+            result2_over_threshold_count_now = 0;
+        end
+
+        if (result2_over_threshold_count_now > result2_count_threshold)
+            result3(i) = -60;
+        else
+            result3(i) = 0;
+        end
+    end
+
+    subplot(figure_row,figure_column,figure_num)
+    plot(inputdata)  %滤波前的时域图
+    hold on
+    plot(after_filter_data,'k') %滤波后的数据
+    hold on
+    plot(result,'r')    
+    hold on
+    plot(result2,'g')
+    hold on
+    plot(result3)
+    hold on
+end
