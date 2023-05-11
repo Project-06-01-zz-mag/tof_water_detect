b894d47450e446a62afa18efb5c1899c5d30c5b3
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-05-11 11:28:43 +0800

    1000到100后33，最终matlab结果和飞机端不同

--- 2_tool_scripe/gettruedata_from_time.m
+++ 2_tool_scripe/gettruedata_from_time.m
@@ -1,13 +1,16 @@
-
-length = size(time,1);
-
+% 从kst中导出数据的时候，要按照时间、dfps、filter、、的顺序排列
+% 才可以用这个脚本自动导出
+time=watertest1.VarName1
+dpfs=watertest1.VarName2
+fly_filter=watertest1.VarName3
 now_time = 1
 j=1
-
+length = size(time,1);
 for i=1:length
     r = isequal(time(i), now_time)
     if(r==0)
         new_fpds(j) = dpfs(i)
+        new_fly_filter(j) = fly_filter(i)
         j=j+1
         now_time = time(i)
     end
--- 3_verify_script/filter_function_verify.m
+++ 3_verify_script/filter_function_verify.m
@@ -5,6 +5,7 @@ close all
 global N
 global a b
 global x y
+global old_new_data
 
 x = [0 0 0]
 y = [0 0 0]
@@ -13,11 +14,14 @@ N = 2;
 b = [0.20169205911526   -0.40338411823052    0.20169205911526 ]
 a = [1                   0.392116929473767   0.198885165934809]
 
-length_end = 100
-
-dpfs_mat_struct_load = load('4_rawdata_fromtime/truedata_fromtime_Asphaltroad3.mat');   
+dpfs_mat_struct_load = load('4_rawdata_fromtime/0_filter_data/officewatertetest2.mat');   
 dpfs_mat_select_water = dpfs_mat_struct_load.new_fpds;
 length_raw = size(dpfs_mat_select_water',1)
+
+dpfs_mat_select_filter_load = load('4_rawdata_fromtime/0_filter_data/officewatertetest2_filter.mat');   
+dpfs_mat_select_filter = dpfs_mat_select_filter_load.new_fly_filter;
+
+length_end = length_raw
 
 for i = 1:length_end
     origin(i) = dpfs_mat_select_water(i)
@@ -25,15 +29,25 @@ for i = 1:length_end
 end
 
 timeview = 1:length_end
-plot(timeview,r,'k') %滤波后的数据
+plot(timeview,r,'r') %滤波后的数据
 hold on
-plot(timeview,origin,'k') %滤波后的数据
+plot(timeview,origin,'g') %原始数据
+hold on
+plot(timeview,dpfs_mat_select_filter,'k') %飞机滤波后的数据
 hold on
 
 function r=filter_hpl(new_data)
     global N
     global a b
     global x y
+    global old_new_data
+    old_new_data = 0;
+    if(new_data<-70||new_data>-5)
+        new_data = old_new_data
+    else
+        old_new_data = new_data
+    end
+
     begin = N +1
     y0= 0
     for i=begin:-1:2 %从3开始到2，但是N是2
