cee55651c61db51382000aaa3907615585f2bb88
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-06-14 11:21:32 +0800

    统一ffc和仿真测试里面code的格式

--- 8mextest/3_water_detect/water_detect_ffc.cpp
+++ 8mextest/3_water_detect/water_detect_ffc.cpp
@@ -1,8 +1,6 @@
 #include "water_detect_ffc.h"
 
-
-
-waterdetectflag_t water_detect::tof_water_detect(waterdetectrawdata_t *waterdetectrawdata) {
+  waterdetectflag_t water_detect::tof_water_detect(waterdetectrawdata_t *waterdetectrawdata) {
     static float dpfs_old = 0;
     static float dpfs_date_cache[WAT_WIN_SIZE] = {0};
     static uint16_t cnt_for_stdvariance = 0;
@@ -11,80 +9,67 @@ waterdetectflag_t water_detect::tof_water_detect(waterdetectrawdata_t *waterdete
     waterdetectflag_t waterdetectflag_temp;
 
     // when fly state change ,reset filter and cache of stdvariance
-if(waterdetectrawdata->fly_state_last != waterdetectrawdata->fly_state_now)
-{
-    waterdetectflag_temp.fly_state_change = true;
-    tofdpfsFilter_.reset(0.f);
-    cnt_for_stdvariance = 0;
-    waterdetectrawdata->time_state_change = waterdetectrawdata->time_input;
-}
-waterdetectrawdata->fly_state_last  = waterdetectrawdata->fly_state_now;
-
-if( waterdetectrawdata->fly_state_now == false )
-{
-    waterdetectrawdata->dpfs_new = 0;
-    dpfs_old =0 ;
-}
-
-if((waterdetectrawdata->time_input - waterdetectrawdata->time_state_change) < 2.0f)
-{
-    water_cnt = 0;
-}
-
-if (waterdetectrawdata->dpfs_new < -70 || waterdetectrawdata->dpfs_new > -5) {
-    waterdetectrawdata->dpfs_new = dpfs_old;
-} else {
-    dpfs_old = waterdetectrawdata->dpfs_new;
-}
-
-waterdetectflag_temp.hpf_dpfs = tofdpfsFilter_.filter(waterdetectrawdata->dpfs_new);
-
-if (cnt_for_stdvariance != WAT_WIN_SIZE - 1) {
-    dpfs_date_cache[cnt_for_stdvariance] = waterdetectflag_temp.hpf_dpfs;
-    cnt_for_stdvariance++;
-} else {
-    for (uint16_t i = 1; i < WAT_WIN_SIZE; i++) {
-    dpfs_date_cache[i - 1] = dpfs_date_cache[i];
+    if(waterdetectrawdata->fly_state_last != waterdetectrawdata->fly_state_now)
+    {
+      waterdetectflag_temp.fly_state_change = true;
+      tofdpfsFilter_.reset(0.f);
+      cnt_for_stdvariance = 0;
+      waterdetectrawdata->time_state_change = waterdetectrawdata->time_input;
     }
-    dpfs_date_cache[WAT_WIN_SIZE - 1] = waterdetectflag_temp.hpf_dpfs;
-}
+    waterdetectrawdata->fly_state_last  = waterdetectrawdata->fly_state_now;
 
-// get mean value
-for (uint16_t i = 0; i < WAT_WIN_SIZE; i++) {
-    waterdetectflag_temp.mean += dpfs_date_cache[i] / WAT_WIN_SIZE;
-}
-
-//get stdvariance
-for (uint16_t i = 0; i < WAT_WIN_SIZE; i++) {
-    waterdetectflag_temp.variance += pow(dpfs_date_cache[i] - waterdetectflag_temp.mean, 2) / WAT_WIN_SIZE;
-}
-waterdetectflag_temp.stdvariance = sqrt(waterdetectflag_temp.variance);
-
-//judge water flag
-if (waterdetectflag_temp.stdvariance > STAND_DEV_VALUE_THRESHOLD) {
-    if (water_cnt < 1000) {
-    water_cnt++;
+    if((waterdetectrawdata->time_input - waterdetectrawdata->time_state_change) < 2.0f)
+    {
+        water_cnt = 0;
     }
-} else {
-    water_cnt = 0;
-}
+    if (waterdetectrawdata->dpfs_new < -70 || waterdetectrawdata->dpfs_new > -5) {
+      waterdetectrawdata->dpfs_new = dpfs_old;
+    } else {
+      dpfs_old = waterdetectrawdata->dpfs_new;
+    }
 
-if (water_cnt > WATER_CNT_THRESHOLD) {
-    waterdetectflag_temp.waterflag = true;
-} else {
-    waterdetectflag_temp.waterflag = false;
-}
+    if( waterdetectrawdata->fly_state_now == false)
+    {
+      waterdetectrawdata->dpfs_new = 0;
+      dpfs_old =0 ;
+    }
+    waterdetectflag_temp.hpf_dpfs = tofdpfsFilter_.filter(waterdetectrawdata->dpfs_new);
 
-return waterdetectflag_temp;
-}
+    if (cnt_for_stdvariance != WAT_WIN_SIZE - 1) {
+      dpfs_date_cache[cnt_for_stdvariance] = waterdetectflag_temp.hpf_dpfs;
+      cnt_for_stdvariance++;
+    } else {
+      for (uint16_t i = 1; i < WAT_WIN_SIZE; i++) {
+        dpfs_date_cache[i - 1] = dpfs_date_cache[i];
+      }
+      dpfs_date_cache[WAT_WIN_SIZE - 1] = waterdetectflag_temp.hpf_dpfs;
+    }
 
-//时间  飞行状态  信号强度  水面识别结果
-//ffcprintf("tof_hpf %8.6f %d %8.6f %d\n", t, waterdetectrawdata_.fly_state_now,waterdetectrawdata_.dpfs_new,waterdetectflag_now.waterflag); // for matlab debug
-// void ParseTof(Devices::mtTimestamp t, const Devices::mtRawRangefinder &raw, bool updated) {
+    // get mean value
+    for (uint16_t i = 0; i < WAT_WIN_SIZE; i++) {
+      waterdetectflag_temp.mean += dpfs_date_cache[i] / WAT_WIN_SIZE;
+    }
 
-//     waterdetectrawdata_.fly_state_now = at_beginning_of_flying;
-//     waterdetectrawdata_.dpfs_new = raw.attach_info.dpfs;
-//     waterdetectflag_now = tof_water_detect(&waterdetectrawdata_);
-//     ffcprintf("tof_hpf %8.6f %d %8.6f %d\n", t, waterdetectrawdata_.fly_state_now,waterdetectrawdata_.dpfs_new,waterdetectflag_now.waterflag); // for matlab debug
-//     tof_.water_flag = waterdetectflag_now.waterflag;
-// }
+    //get stdvariance
+    for (uint16_t i = 0; i < WAT_WIN_SIZE; i++) {
+      waterdetectflag_temp.variance += pow(dpfs_date_cache[i] - waterdetectflag_temp.mean, 2) / WAT_WIN_SIZE;
+    }
+    waterdetectflag_temp.stdvariance = sqrt(waterdetectflag_temp.variance);
+
+    //judge water flag
+    if (waterdetectflag_temp.stdvariance > STAND_DEV_VALUE_THRESHOLD) {
+      if (water_cnt < 1000) {
+        water_cnt++;
+      }
+    } else {
+      water_cnt = 0;
+    }
+
+    if (water_cnt > WATER_CNT_THRESHOLD) {
+      waterdetectflag_temp.waterflag = true;
+    } else {
+      waterdetectflag_temp.waterflag = false;
+    }
+
+    return waterdetectflag_temp;
+  }
--- 8mextest/3_water_detect/water_detect_ffc.h
+++ 8mextest/3_water_detect/water_detect_ffc.h
@@ -82,28 +82,28 @@ class TofHpfFilter : public DigiFilterBase<float> {
   TofHpfFilter() { init(N, num, den); }
 };
 
-#define WAT_WIN_SIZE 40
-#define STAND_DEV_VALUE_THRESHOLD 0.5
-#define WATER_CNT_THRESHOLD 40
 
-typedef struct waterdetectflag_s {
-float mean = 0;
-float variance = 0;
-float stdvariance = 0;
-float hpf_dpfs;
-bool waterflag = false;
-bool fly_state_change = false;
-} waterdetectflag_t;
+  #define WAT_WIN_SIZE 40
+  #define STAND_DEV_VALUE_THRESHOLD 0.5
+  #define WATER_CNT_THRESHOLD 40
 
-typedef struct waterdetectrawdata_s {
-bool fly_state_now;
-bool fly_state_last = false;
-float dpfs_new;
-double time_input;
-double time_state_change;
-} waterdetectrawdata_t;
+  typedef struct waterdetectflag_s {
+    float mean = 0;
+    float variance = 0;
+    float stdvariance = 0;
+    float hpf_dpfs;
+    bool waterflag = false;
+    bool fly_state_change = false;
+  } waterdetectflag_t;
 
-
+    typedef struct waterdetectrawdata_s {
+    bool fly_state_now;
+    bool fly_state_last = false;
+    float dpfs_new;
+    double time_input;
+    double time_state_change;
+  } waterdetectrawdata_t;
+  
 class water_detect
 {
 public:
